6de5bb7ddc182cb422a7efe83bdc4a906c4f693f
yangling01 <yangling01@diditaxi.com.cn>
2015-07-17 15:47:44 +0800

    1212

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from bson import json_util
 from bson.objectid import ObjectId
 
 
-MONGODB_DB_URL = os.environ.get('27017/tcp') if os.environ.get('27017/tcp').replace('tcp','mongodb') else 'mongodb://localhost:27017/'
+MONGODB_DB_URL = os.environ.get('27017/tcp').replace('tcp','mongodb') if os.environ.get('27017/tcp') else 'mongodb://localhost:27017/'
 MONGODB_DB_NAME = os.environ.get('OPENSHIFT_APP_NAME') if os.environ.get('OPENSHIFT_APP_NAME') else 'getRequires'
 
 client = MongoClient(MONGODB_DB_URL)
